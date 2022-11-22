19d37e16dad7501aac4976abb589a7b9ddc64b1e
Noah Paige <noahlandonpaige@gmail.com>
2022-11-22 09:28:51 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1 className="text-3xl bg-blue-200 font-bold underline">
+      Hello world!
+      </h1>
+      <div className="rating gap-1">
+  <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" checked />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+  <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+</div>
+    </div>
+    
+  )
+}
+
+export default App
