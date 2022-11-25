0af6855c85bc841a257e0f556f19bcc8926cea33
Noah Paige <noahlandonpaige@gmail.com>
2022-11-24 20:34:49 -0500

    waaaaaavy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,5 +8,17 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    darkTheme: "dark",
+  }
 }
